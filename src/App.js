0b2da4ce9a9a43b391fe64653f9d5eaf3e2e8bfc
Anmol Dubey <anmoldubey62@gmail.com>
2019-04-11 12:38:13 +0530

    initial project created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        example
+      </div>
+    );
+  }
+}
+
+export default App;
